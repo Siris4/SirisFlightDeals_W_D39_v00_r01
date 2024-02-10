78256f0d7af67248b01770b1d913f25073ed1dc7
Gavin Martin <97780529+Siris4@users.noreply.github.com>
2024-02-09 19:59:52 -0800

    Add files via upload

---
+++ main_W_D39_v00_r01.py
@@ -0,0 +1,9 @@
+#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+from pprint import pprint
+from SirisFlightDeals_Sheety_JSON_Data_in_Py_Format import sheet_data
+
+# TODO: Authenticate with a Bearer Token
+# TODO: Ensure all sensitive data is extracted and created into an Environment Variable, above here.
+
+sheet_data
